700eebfd0ab836ca652999c5c3e06b629bcbdcab
ismael-creator <ismabarri41@gmail.com>
2020-09-01 18:07:31 -0300

    Updates

---
+++ precache-manifest.bdf028a1ac8f2b5ea6106f7f5d1bcd96.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6082aaf05b21deb106dec6d229d4d72f",
+    "url": "/GifExpertApp/index.html"
+  },
+  {
+    "revision": "ae76c3c07d3e243731ae",
+    "url": "/GifExpertApp/static/css/main.fa6f670d.chunk.css"
+  },
+  {
+    "revision": "51858a585169eeb49f95",
+    "url": "/GifExpertApp/static/js/2.56c56de3.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/GifExpertApp/static/js/2.56c56de3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ae76c3c07d3e243731ae",
+    "url": "/GifExpertApp/static/js/main.859af4e5.chunk.js"
+  },
+  {
+    "revision": "23ec801f39a39732997e",
+    "url": "/GifExpertApp/static/js/runtime-main.5e7f0275.js"
+  }
+]);
